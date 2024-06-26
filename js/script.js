1bc2312f0cf2c6f1423b49f8993b5d21f9800a7a
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2024-06-26 03:09:34 +0300

    Styles: footer

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ const navLinks = document.querySelector('.navbar__links');
 navToggleBtn.addEventListener('click', ( e => {
     navLinks.classList.toggle('navbar__links--visible');
 }))
+
+
+// update copyright year automatically
+// get year updated using getFullYear() js function
+const copyrightYear = document.getElementById('js-update-year');
+const theCurrentDate = new Date();
+
+const theCurrentYear = theCurrentDate.getFullYear();
+copyrightYear.textContent = theCurrentYear;
+
